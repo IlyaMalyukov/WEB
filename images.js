5d57c77d696c77c0873e7c5556f1b3ce6e1d2c19
IlyaMalyukov <ilya.maliukov@yandex.ru>
2021-03-13 01:20:32 +0300

    add home aimation

---
+++ images.js
@@ -0,0 +1,34 @@
+const home_images = [
+  'img/home_1/home_1.png',
+  'img/home_2/home_2.png',
+  'img/home_3/home_3.png',
+  'img/home_4/home_4.png'
+];
+
+const floral_images = [
+  'img/home_1/florals_1.png',
+  'img/home_2/florals_1.png',
+  'img/home_3/florals_1.png',
+  'img/home_4/florals_1.png'
+];
+
+const geometry_images = [
+  'img/home_1/geometric_shapes_1.png',
+  'img/home_2/geometric_shapes_1.png',
+  'img/home_3/geometric_shapes_1.png',
+  'img/home_4/geometric_shapes_1.png'
+];
+
+const geometry_images_second = [
+  'img/home_1/geometric_shapes_2.png',
+  'img/home_2/geometric_shapes_2.png',
+  'img/home_3/geometric_shapes_2.png',
+  'img/home_4/geometric_shapes_2.png'
+];
+
+const geometry_images_second_s = [
+  'img/home_1/geometric_shapes_2_1.png',
+  'img/home_2/geometric_shapes_2_1.png',
+  'img/home_3/geometric_shapes_2_1.png',
+  'img/home_4/geometric_shapes_2_1.png'
+];
--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-barba.init();
+let pos = 1;
+
+function slideShow() {
+  
+  if(pos >= 4) {
+    pos = 0;
+  };
+
+  switch(pos) {
+    case 0:
+      homeFirst.style.display = 'block';
+      homeSecond.style.display = 'none';
+      homeThird.style.display = 'none';
+      homeFourth.style.display = 'none';
+      console.log(pos)
+    break;
+    case 1:
+      homeFirst.style.display = 'none';
+      homeSecond.style.display = 'block';
+      homeThird.style.display = 'none';
+      homeFourth.style.display = 'none';
+      console.log(pos)  
+    break;
+    case 2:
+      homeFirst.style.display = 'none';
+      homeSecond.style.display = 'none';
+      homeThird.style.display = 'block';
+      homeFourth.style.display = 'none';
+      console.log(pos)  
+    break;
+    case 3:
+      homeFirst.style.display = 'none';
+      homeSecond.style.display = 'none';
+      homeThird.style.display = 'none';
+      homeFourth.style.display = 'block';
+      console.log(pos)  
+    break;
+  }
+  pos++;
+
+};
+
+// JQuery
+
+function animation(){
+  $(imgs).fadeOut(2000).fadeIn(2000);
+};
+
+// Barba.JS
+
+barba.init({
+  transitions: [{
+    once() {
+
+    setInterval(() => slideShow(), 4000) 
+    setInterval("animation()", 2000);
+    }
+  }]
+});
